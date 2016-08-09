3ad73c04f27ab95b657f358eee256191dc5c62aa
ilya <ilma@e11it.ru>
2016-08-09 16:08:20 +0300

    Wait for DB to startup

--- entripoint.sh
+++ entripoint.sh
@@ -2,6 +2,14 @@
 
 set -o pipefail -u
 
+# Wait for the db service to be ready before continuing
+echo "waiting for db..."  
+while ! nc -w 1 -z $DB_HOST $DB_PORT 2>/dev/null;  
+do  
+  echo -n .
+  sleep 1
+done
+
 rake db:exists && rake db:migrate || rake db:setup
 
 exec /usr/bin/supervisord -nc /etc/supervisor/supervisord.conf
